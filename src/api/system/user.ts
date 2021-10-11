36ee0e83a378144271963ff859d018d9615a6e01
weiyupeng <weiyupengself@163.com>
2021-10-11 13:58:48 +0800

    还原

--- src/api/system/user.ts
+++ src/api/system/user.ts
@@ -28,7 +28,7 @@ export function getUserInfo() {
 export function login(params) {
   return http.request<BasicResponseModel>(
     {
-      url: '/token/',
+      url: '/login',
       method: 'POST',
       params,
     },

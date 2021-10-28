24d821a7747d1efb6290076254047144fd4dd5c8
weiyupeng <weiyupengself@163.com>
2021-10-28 14:05:58 +0800

    增加配置

--- src/views/homeStatic/Admin.vue
+++ src/views/homeStatic/Admin.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
-    超管首页
+    <p class="welcomeText">你好{{ username }}，欢迎光临！</p>
   </div>
 </template>
 
 <script lang="ts" setup>
+  import { useUserStore } from '@/store/modules/user';
+  const userStore = useUserStore();
+  const { username } = userStore?.info || {};
 </script>
 
 <style lang="less" scoped>
+  .welcomeText{
+    font-size: 30px;
+    color: #000;
+  }
 </style>
